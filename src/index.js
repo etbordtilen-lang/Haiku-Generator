e25558977d7b899d2bcc4857ab89ff4bf9b840be
etbordtilen-lang <etbordtilen@gmail.com>
2026-01-10 22:33:59 +0100

    Added initial search and output function
    Added typewriter plugin for haiku output

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+function generateHaiku(event) {
+  event.preventDefault();
 
+  new Typewriter("#haiku-output", {
+    strings: `Frostbit finger tips <br />
+    Fairy light canoes sailing by <br />
+    Lukewarm frankfurter`,
+    autoStart: true,
+    delay: 80,
+    cursor: "",
+  });
+
+  //alert("Haiku generation is not yet implemented.");
+}
+
+let haikuFormElement = document.querySelector("#haiku-generator-form");
+haikuFormElement.addEventListener("submit", generateHaiku);
