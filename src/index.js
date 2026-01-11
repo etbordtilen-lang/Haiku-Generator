1a403cff143fc34345431afdb26d1c1203e8cfc4
etbordtilen-lang <etbordtilen@gmail.com>
2026-01-11 22:50:45 +0100

    Updated index.js adding API
    Linking user input with Haiku output. Updating speed typewriter is typed.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+function displayHaiku(response) {
+  let haiku = response.data.answer;
+  let formattedHaiku = haiku.replace(/\n/g, "<br />");
+  new Typewriter("#haiku-output", {
+    strings: formattedHaiku,
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
 function generateHaiku(event) {
   event.preventDefault();
 
-  new Typewriter("#haiku-output", {
-    strings: `Frostbit finger tips <br />
-    Fairy light canoes sailing by <br />
-    Lukewarm frankfurter`,
-    autoStart: true,
-    delay: 80,
-    cursor: "",
-  });
+  let topicInputElement = document.querySelector("#user-topic-input");
+  let apiKey = "fe3at4b9ob076b4e8c6aecc0604ae6d6";
+  let prompt = `Generate a haiku about ${topicInputElement.value}`;
+  let context =
+    "Haiku poetry, nature, seasons, emotions. In Haiku format: 3 lines, 5-7-5 syllables.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating haiku...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayHaiku);
 
   //alert("Haiku generation is not yet implemented.");
 }
 
 let haikuFormElement = document.querySelector("#haiku-generator-form");
 haikuFormElement.addEventListener("submit", generateHaiku);
+
+//  let haikuOutputElement = document.querySelector("#haiku-output");
+//haikuOutputElement.innerHTML = response.data.generated_text;
