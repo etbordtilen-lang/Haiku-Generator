69d220c12fd1a3f0ddb68c696f0f7e42705d85e5
etbordtilen-lang <etbordtilen@gmail.com>
2026-01-11 23:23:35 +0100

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function displayHaiku(response) {
   new Typewriter("#haiku-output", {
     strings: formattedHaiku,
     autoStart: true,
-    delay: 40,
+    delay: 70,
     cursor: "",
   });
 }
@@ -16,12 +16,15 @@ function generateHaiku(event) {
   let apiKey = "fe3at4b9ob076b4e8c6aecc0604ae6d6";
   let prompt = `Generate a haiku about ${topicInputElement.value}`;
   let context =
-    "Haiku poetry, nature, seasons, emotions. In Haiku format: 3 lines, 5-7-5 syllables.";
+    "Haiku poetry, nature, seasons, emotions. In Haiku format: 3 lines, 5-7-5 syllables. Remove punctuation.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating haiku...");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let haikuOutputElement = document.querySelector("#haiku-output");
+  haikuOutputElement.innerHTML = `Generating a Haiku about "${topicInputElement.value}"...`;
+
+  //console.log("Generating haiku...");
+  //console.log(`Prompt: ${prompt}`);
+  //console.log(`Context: ${context}`);
 
   axios.get(apiURL).then(displayHaiku);
 
